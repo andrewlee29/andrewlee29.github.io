7481f5eacdb22c5f0655b742624d2ff99b858b17
andrewlee29 <andrewlee201.al@gmail.com>
2021-04-30 21:41:41 -0400

    added text animation

--- js/custom.js
+++ js/custom.js
@@ -16,5 +16,124 @@ document.addEventListener('DOMContentLoaded', function() {
       .pauseFor(2500)
       .start();
 
+    //text animation
+    gsap.registerPlugin(ScrollTrigger);
+    if(window.innerWidth >800)
+    {
+        //about
+        gsap.from('.person', {
+            scrollTrigger: {
+                trigger:'.person',
+                toggleActions: "restart",
+                start: "5px 50%",
+                end:"+=100px",
+                scrub: 1,
+            },
+            x: '100vh',
+            rotation:360,
+            opacity: 0,
+            duration: 2
+        });
+        gsap.from('.abouttext', {
+            scrollTrigger: {
+                trigger:'.abouttext',
+                toggleActions: "restart",
+                start: "5px 50%",
+                end:"+=100px",
+                scrub: 1,
+            },
+            x: '-100vh',
+            rotation:360,
+            opacity: 0,
+            duration: 2
+        });
+        //skill
+        gsap.from('.software', {
+            scrollTrigger: {
+                trigger:'.software',
+                toggleActions: "restart",
+                start: "10px 50%",
+                end:"+=100px",
+                scrub: 1,
+            },
+            x: '-100vh',
+            opacity: 0,
+            duration: 2
+        });
+        gsap.from('.language', {
+            scrollTrigger: {
+                trigger:'.language',
+                toggleActions: "restart",
+                start: "30px 50%",
+                end:"+=100px",
+                scrub: 1,
+            },
+            x: '-100vh',
+            opacity: 0,
+            duration: 2,
+        });
+        gsap.from('.os', {
+            scrollTrigger: {
+                trigger:'.os',
+                toggleActions: "restart",
+                start: "30px 50%",
+                end:"+=100px",
+                scrub: 1,
+            },
+            x: '100vh',
+            opacity: 0,
+            duration: 2
+        });
+        gsap.from('.softskill', {
+            scrollTrigger: {
+                trigger:'.softskill',
+                toggleActions: "restart",
+                start: "10px 50%",
+                end:"+=100px",
+                scrub: 1,
+            },
+            x: '100vh',
+            opacity: 0,
+            duration: 2,
+        });
+        //project
+        gsap.from('.pj1', {
+            scrollTrigger: {
+                trigger:'.pj1',
+                toggleActions: "restart",
+                start: "10px 50%",
+                end:"+=100px",
+                scrub: 1,
+            },
+            y: '-10vh',
+            opacity: 0,
+            duration: 2,
+        });
+        gsap.from('.pj2', {
+            scrollTrigger: {
+                trigger:'.pj2',
+                toggleActions: "restart",
+                start: "10px 70%",
+                end:"+=100px",
+                scrub: 1,
+            },
+            y: '10vh',
+            opacity: 0,
+            duration: 2,
+        });
+        gsap.from('.pj3', {
+            scrollTrigger: {
+                trigger:'.pj3',
+                toggleActions: "restart",
+                start: "10px 50%",
+                end:"+=100px",
+                scrub: 1,
+            },
+            y: '-10vh',
+            opacity: 0,
+            duration: 2,
+        });
+    }
+
 
   });
